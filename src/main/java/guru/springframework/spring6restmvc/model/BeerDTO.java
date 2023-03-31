cd970fa65f5aa431ff3be4c5e7b23c553be8cac1
Carlos Tadeu <carlossantos@opus-software.com.br>
2023-03-31 15:25:59 -0300

    adding beer validation

--- src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,13 +16,20 @@ public class BeerDTO {
     private UUID id;
     private Integer version;
 
-    @NotBlank
-    @NotNull
+    @NotBlank @NotNull
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotBlank @NotNull
     private String upc;
+
     private Integer quantityOnHand;
+
+    @NotNull @Positive
     private BigDecimal price;
+
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
 }
