7c5be747d9f8b9833dd8e896ff0045189bb3361e
Carlos Tadeu <carlossantos@opus-software.com.br>
2023-03-31 15:39:11 -0300

    adding jpa validation

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,12 @@ class BeerRepositoryTest {
     void saveCustomer() {
         Beer beer = beerRepository.save(Beer.builder()
                 .beerName("My New Beer")
+                .beerStyle(BeerStyle.IPA)
+                .upc("01")
+                .price(BigDecimal.ONE)
                 .build());
+
+        beerRepository.flush();
 
         assertThat(beer).isNotNull();
         assertThat(beer.getId()).isNotNull();
