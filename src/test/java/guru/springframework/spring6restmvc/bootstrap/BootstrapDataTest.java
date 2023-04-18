a9fe5dcb687647619f02da2d46e6a40829073fe9
Carlos Tadeu <carlossantos@opus-software.com.br>
2023-04-18 14:46:09 -0300

    fixing integration test

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -2,14 +2,18 @@ package guru.springframework.spring6restmvc.bootstrap;
 
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import guru.springframework.spring6restmvc.services.BeerCsvService;
+import guru.springframework.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
     @Autowired
@@ -18,17 +22,20 @@ class BootstrapDataTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    BeerCsvService beerCsvService;
+
     BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, beerCsvService);
     }
 
     @Test
     void run() throws Exception {
         bootstrapData.run();
-        assertThat(beerRepository.count()).isEqualTo(1);
-        assertThat(customerRepository.count()).isEqualTo(1);
+        assertThat(beerRepository.count()).isPositive();
+        assertThat(customerRepository.count()).isPositive();
     }
 }
