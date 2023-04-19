05efefe87c2bb4db52c7bc0fa080b2204c4598e2
Carlos Tadeu <carlossantos@opus-software.com.br>
2023-04-19 14:40:08 -0300

    adding listBeers spring mvc test

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -27,6 +27,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -53,6 +54,14 @@ class BeerControllerIT {
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void listBeersByName() throws Exception {
+        mockMvc.perform(get(BeerController.BEER_PATH)
+                .queryParam("beerName", "IPA"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", Is.is(100)));
     }
 
     @Test
